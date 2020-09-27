6269af7cd40123342f2f759d4fa95fade8307c42
LayneHansen <layne.d.hansen@gmail.com>
2020-09-26 21:42:45 -0600

    got all tests to work

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const render = require("./lib/htmlRenderer.js");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/html.Renderer.js");
 
 
 // Write code to use inquirer to gather information about the development team members,
@@ -67,6 +67,7 @@ await inquirer
 start();
 
 function writeToFile(fileName, data) {
+    console.log(writeToFile);
     fs.writeFile(fileName, data, function(err) {
 
         if (err) {
